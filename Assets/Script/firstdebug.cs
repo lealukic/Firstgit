1a11c112d98adced854087eef01beaf0703d035d
lealukic <lea.lukic@htl-salzburg.ac.at>
2019-10-03 10:10:12 +0200

    added assets and projectsettings with unity

---
+++ Assets/Script/firstdebug.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class firstdebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First git");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
